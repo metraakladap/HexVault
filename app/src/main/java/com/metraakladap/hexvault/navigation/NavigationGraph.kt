abfab60709962e6a691f9aa2da923b789ae37e8e
metraakladap <artpadan@gmail.com>
2025-08-18 17:48:06 +0300

    updated navigation

--- app/src/main/java/com/metraakladap/hexvault/navigation/NavigationGraph.kt
+++ app/src/main/java/com/metraakladap/hexvault/navigation/NavigationGraph.kt
@@ -1,6 +1,7 @@
 package com.metraakladap.hexvault.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
@@ -8,8 +9,10 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.metraakladap.hexvault.screens.LoadingScreen
 import com.metraakladap.hexvault.screens.MainScreen
+import com.metraakladap.hexvault.screens.OnboardingScreen
 import com.metraakladap.hexvault.viewmodel.LoadingScreenViewModel
 import com.metraakladap.hexvault.viewmodel.MainViewModel
+import com.metraakladap.hexvault.viewmodel.OnboardingViewModel
 
 @Composable
 fun NavigationGraph(navController: NavHostController) {
@@ -21,6 +24,19 @@ fun NavigationGraph(navController: NavHostController) {
                  navController.getBackStackEntry(navController.graph.id)
              }
              val viewModel = hiltViewModel<LoadingScreenViewModel>(parent)
+             LaunchedEffect(Unit) {
+                 viewModel.initLoad { shouldOnboard ->
+                     if (shouldOnboard) {
+                         navController.navigate(Screens.Onboarding.route) {
+                             popUpTo(Screens.Loading.route) { inclusive = true }
+                         }
+                     } else {
+                         navController.navigate(Screens.Main.route) {
+                             popUpTo(Screens.Loading.route) { inclusive = true }
+                         }
+                     }
+                 }
+             }
              LoadingScreen(viewModel)
          }
 
@@ -31,6 +47,18 @@ fun NavigationGraph(navController: NavHostController) {
              }
              val viewModel = hiltViewModel<MainViewModel>(parent)
              MainScreen(viewModel)
+         }
+
+         composable(Screens.Onboarding.route) { backStack ->
+             val parent = remember(backStack) {
+                 navController.getBackStackEntry(navController.graph.id)
+             }
+             val viewModel = hiltViewModel<OnboardingViewModel>(parent)
+             OnboardingScreen(viewModel) {
+                 navController.navigate(Screens.Main.route) {
+                     popUpTo(Screens.Loading.route) { inclusive = true }
+                 }
+             }
          }
      }
 }
