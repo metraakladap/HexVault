3e4de27a911cdb6f55fbfffeb5598627de1c744e
metraakladap <artpadan@gmail.com>
2025-08-18 17:40:45 +0300

    added AppModule.kt for DI

---
+++ app/src/main/java/com/metraakladap/hexvault/di/AppModule.kt
@@ -0,0 +1,22 @@
+package com.metraakladap.hexvault.di
+
+import android.content.Context
+import com.metraakladap.hexvault.crypto.SeedManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideSeedManager(@ApplicationContext context: Context): SeedManager =
+        SeedManager(context)
+}
+
+
