907c497cde9f57f55f5d7628b1fefe36ffa5266f
metraakladap <artpadan@gmail.com>
2025-08-18 18:18:50 +0300

    added datastore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.appcompat)
+    implementation(libs.datastore.preferences)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
